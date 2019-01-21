094e8731b1b8a10b7e5c98a496f4c32f563da51d
Dnny <mcmillandanny@gmail.com>
2019-01-20 19:42:28 -0500

    fixed some styles/working on mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,17 +1,13 @@
-'use strict';
+"use strict";
 
 console.log("Helllo from the console!");
 
 var hamburger = document.getElementById('hamburger');
-var lines = document.querySelectorAll('.hamburger-lines');
 
 function hamburgerToggle() {
     lines.forEach(function (line) {
         line.classList.toggle("change");
     });
-    // navOpen.style.display = navOpen.style.display === 'block' ? '' : 'block'; 
-    // navOpen.classList.add("menu-ani")
 };
-
 hamburger.addEventListener("click", hamburgerToggle);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,10 @@
 console.log("Helllo from the console!");
 
-
 const hamburger = document.getElementById('hamburger');
-const lines = document.querySelectorAll('.hamburger-lines');
-
 
 function hamburgerToggle() {
     lines.forEach(function(line) {
         line.classList.toggle("change");       
     });
-    // navOpen.style.display = navOpen.style.display === 'block' ? '' : 'block'; 
-    // navOpen.classList.add("menu-ani")
 };
-
 hamburger.addEventListener("click", hamburgerToggle);
