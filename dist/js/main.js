0cdbed096a65c411670918af7002879f3b7529aa
Dnny <mcmillandanny@gmail.com>
2019-02-06 23:00:05 -0500

    added text carousel function

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,17 +16,46 @@ function hamburgerToggle() {
 };
 hamburger.addEventListener("click", hamburgerToggle);
 
-var text = document.querySelectorAll('.text-car');
+var animateText = new TimelineMax({});
 
+function animateTextInOut() {
+    animateText.from(".text-car", .3, {
+        y: "-80px",
+        ease: Ease.in
+    });
+}
+
+var text = document.querySelectorAll('.text-car');
 var currentTextIndex = 0;
 
 setInterval(function () {
+    animateTextInOut();
 
+    var hideShowingText = function hideShowingText() {
+        var activeText = document.querySelector('.showing');
+
+        if (activeText) {
+            activeText.classList.remove('showing');
+        }
+    };
+
+    var showNewText = function showNewText() {
+        text[currentTextIndex].classList.add('showing');
+    };
+
+    var next = function next() {
+        hideShowingText();
+        currentTextIndex++;
+        if (currentTextIndex === text.length) {
+            currentTextIndex = 0;
+        }
+        showNewText();
+    };
     // find .showing
     // hide the one that's currently .showing
 
     // currentTextIndex++
     // add .showing to text[currentTextIndex] 
-
-}, 5000);
+    next();
+}, 3000);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -15,19 +15,52 @@ function hamburgerToggle() {
 };
 hamburger.addEventListener("click", hamburgerToggle);
 
-let text = document.querySelectorAll('.text-car');
 
+
+let animateText = new TimelineMax({});
+
+function animateTextInOut() {
+    animateText.from(".text-car", .3, {
+        y: "-80px", 
+        ease: Ease.in
+    })
+
+}
+
+let text = document.querySelectorAll('.text-car');
 let currentTextIndex = 0;
 
 setInterval(() => {
+    animateTextInOut();
 
-    // find .showing
+    let hideShowingText = function() {
+        let activeText = document.querySelector('.showing');
+        
+        if (activeText) {
+            activeText.classList.remove('showing');
+        }
+    }
+
+    let showNewText = function() {
+        text[currentTextIndex].classList.add('showing');
+       
+    }
+
+    let next = function() {
+        hideShowingText()
+        currentTextIndex++
+        if (currentTextIndex === text.length) {
+			currentTextIndex = 0;
+		}
+		showNewText()
+    }
+		     // find .showing
     // hide the one that's currently .showing
-    
+
     // currentTextIndex++
     // add .showing to text[currentTextIndex] 
-
-}, 5000)
+    next()
+}, 3000)
 
 
 
